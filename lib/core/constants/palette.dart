55b990c8ad36457674ec1f5712a50eed8213e89d
saied nasiripour <saied.nasiripour@gmail.com>
2023-12-15 08:26:47 +0330

    add Palette (the range of colours used in the decoration) to project

---
+++ lib/core/constants/palette.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const Color bgColor = Color(0xff1F212C);
+
+  static const Color bgDarkColor = Color(0xff070B11);
+  static const Color whiteColor = Color(0xffFFFFFF);
+  static const Color sliderColor = Color(0xff7E70FF);
+  static const Color buttonColor = Color(0xff070B11);
+}
